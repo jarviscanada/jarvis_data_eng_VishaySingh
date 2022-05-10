57a624e1fa5a8a4ff688e0ddd1fe888d83ed9085
Vishay Singh <vishaysingh35@gmail.com>
2022-05-10 20:10:58 +0000

    Finish 1 and 2

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,4 +1,6 @@
--- 1. Group hosts by hardware info
+-- 1. Group hosts by hardware info --
+
+-- Query for retrieving grouped hw info
 SELECT cpu_number, id, total_mem
 FROM host_info
 ORDER BY MAX(cpu_number) OVER (
@@ -6,7 +8,8 @@ ORDER BY MAX(cpu_number) OVER (
     ORDER BY total_mem DESC
     );
 
--- 2. Average memory usage
+-- 2. Average memory usage --
+
 -- Function is taken from SQL hint
 CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
 $$
@@ -16,18 +19,25 @@ END;
 $$
     LANGUAGE PLPGSQL;
 
+-- Temp table to organize steps a little; round the timestamps first
 CREATE TEMP TABLE host_usage_rounded AS
 SELECT host_id, memory_free, timestamp, round5(timestamp) as rounded_time
 FROM host_usage;
 
-SELECT hur.host_id, hin.hostname, hur.rounded_time, AVG((hin.total_mem - hur.memory_free) / hin.total_mem) AS avg_used_mem_percentage
+-- Actual query to retrieve avg mem usage
+SELECT hur.host_id, hin.hostname, hur.rounded_time,
+       FLOOR(AVG((hin.total_mem - hur.memory_free)*1.0 / hin.total_mem)*100) AS avg_used_mem_percentage
 FROM host_info hin INNER JOIN host_usage_rounded hur ON hin.id = hur.host_id
-GROUP BY hur.host_id, hin.hostname, hur.rounded_time;
+GROUP BY hur.host_id, hin.hostname, hur.rounded_time
+ORDER BY hur.host_id, hur.rounded_time;
 
+-- 3. Detect host failure --
 
-SELECT hur.host_id, hin.hostname, hur.rounded_time, hin.total_mem, hur.memory_free,
-       AVG((hin.total_mem - hur.memory_free)) OVER (
-           PARTITION BY hur.rounded_time, hur.host_id
-           ) AS avg_used_mem_percentage
-FROM host_info hin INNER JOIN host_usage_rounded hur ON hin.id = hur.host_id;
+-- Query for finding failed hosts
+SELECT hur.host_id, hur.rounded_time as rounded_time, COUNT(*) as num_data_points
+FROM host_info hin INNER JOIN host_usage_rounded hur ON hin.id = hur.host_id
+GROUP BY hur.host_id, hur.rounded_time
+HAVING COUNT(*) < 3;
 
+-- Clean up
+DROP TABLE IF EXISTS host_usage_rounded;
