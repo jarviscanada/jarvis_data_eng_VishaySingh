0b03bc5802c5c81172015308b93f593943f13109
Vishay Singh <vishaysingh35@gmail.com>
2022-06-01 16:30:29 +0000

    Finish ArrayJList

--- practice/src/main/java/ca/jrvs/practice/dataStructure/HelloWorld.java
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/HelloWorld.java
@@ -1,10 +1,18 @@
 package ca.jrvs.practice.dataStructure;
 
+import ca.jrvs.practice.dataStructure.list.ArrayJLists;
+import java.util.ArrayList;
+import java.util.List;
+
 class HelloWorld {
 
   // Your program begins with a call to main().
   // Prints "Hello, World" to the terminal window.
   public static void main(String args[]) {
     System.out.println("Hello, World");
+    List<String> arr = new ArrayList<>();
+    System.out.println(arr.size());
+    arr.add("Mark");
+    System.out.println(arr.size());
   }
 }
---
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/list/ArrayJLists.java
@@ -0,0 +1,203 @@
+package ca.jrvs.practice.dataStructure.list;
+
+public class ArrayJLists<E> implements JList<E>{
+
+  /**
+   * Default initial capacity.
+   */
+  private static final int DEFAULT_CAPACITY = 10;
+
+  /**
+   * The array buffer into which the elements of the ArrayList are stored.
+   * The capacity of the ArrayList is the length of this array buffer.
+   */
+  transient Object[] elementData; // non-private to simplify nested class access
+  /**
+   * The size of the ArrayList (the number of elements it contains).
+   */
+  private int size;
+
+  /**
+   * Constructs an empty list with the specified initial capacity.
+   *
+   * @param  initialCapacity  the initial capacity of the list
+   * @throws IllegalArgumentException if the specified initial capacity
+   *         is negative
+   */
+  public ArrayJLists(int initialCapacity) {
+    if (initialCapacity > 0) {
+      this.elementData = new Object[initialCapacity];
+      this.size = 0;
+    } else {
+      throw new IllegalArgumentException("Illegal Capacity: " +
+          initialCapacity);
+    }
+  }
+
+  /**
+   * Constructs an empty list with an initial capacity of ten.
+   */
+  public ArrayJLists() {
+    this(DEFAULT_CAPACITY);
+  }
+
+  /**
+   * Increases the capacity of this <tt>ArrayJList</tt> instance, if
+   * necessary, to ensure  that it can hold at least the number of elements
+   * specified by the minimum capacity argument.
+   *
+   * @param   minCapacity   the desired minimum capacity.
+   */
+  public void ensureCapacity(int minCapacity) {
+    int oldCapacity = elementData.length;
+    if (minCapacity > oldCapacity) {
+      Object oldData[] = elementData;
+      int newCapacity = (oldCapacity * 3)/2 + 1;
+      if (newCapacity < minCapacity){
+        newCapacity = minCapacity;
+      }
+      elementData = new Object[newCapacity];
+      System.arraycopy(oldData, 0, elementData, 0, size);
+    }
+  }
+
+  /**
+   * Appends the specified element to the end of this list (optional operation).
+   *
+   * @param e element to be appended to this list
+   * @return <tt>true</tt> (as specified by Collection#add})
+   * @throws NullPointerException if the specified element is null and this list does not permit
+   *                              null elements
+   */
+  @Override
+  public boolean add(E e) {
+    ensureCapacity(size + 1);
+    elementData[size++] = e;
+    return true;
+  }
+
+  /**
+   * Returns an array containing all of the elements in this list in proper sequence (from first to
+   * last element).
+   *
+   * <p>This method acts as bridge between array-based and collection-based
+   * APIs.
+   *
+   * @return an array containing all of the elements in this list in proper sequence
+   */
+  @Override
+  public Object[] toArray() {
+    Object[] arr = new Object[size];
+    System.arraycopy(elementData, 0, arr, 0, size);
+    return arr;
+  }
+
+  /**
+   * The size of the ArrayList (the number of elements it contains).
+   */
+  @Override
+  public int size() {
+    return size;
+  }
+
+  /**
+   * Returns <tt>true</tt> if this list contains no elements.
+   *
+   * @return <tt>true</tt> if this list contains no elements
+   */
+  @Override
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * Returns the index of the first occurrence of the specified element in this list, or -1 if this
+   * list does not contain the element. More formally, returns the lowest index <tt>i</tt> such
+   * that
+   * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>,
+   * or -1 if there is no such index.
+   *
+   * @param o
+   */
+  @Override
+  public int indexOf(Object o) {
+    if (o == null) {
+      for (int i = size-1; i >= 0; i--)
+        if (elementData[i]==null)
+          return i;
+    } else {
+      for (int i = size-1; i >= 0; i--)
+        if (o.equals(elementData[i]))
+          return i;
+    }
+    return -1;
+  }
+
+  /**
+   * Returns <tt>true</tt> if this list contains the specified element. More formally, returns
+   * <tt>true</tt> if and only if this list contains at least one element <tt>e</tt> such that
+   * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+   *
+   * @param o element whose presence in this list is to be tested
+   * @return <tt>true</tt> if this list contains the specified element
+   * @throws NullPointerException if the specified element is null and this list does not permit
+   *                              null elements
+   */
+  @Override
+  public boolean contains(Object o) {
+    return this.indexOf(o) != -1;
+  }
+
+  /**
+   * Returns the element at the specified position in this list.
+   *
+   * @param index index of the element to return
+   * @return the element at the specified position in this list
+   * @throws IndexOutOfBoundsException if the index is out of range (<tt>index &lt; 0 || index &gt;=
+   *                                   size()</tt>)
+   */
+  @Override
+  public E get(int index) {
+    if (index <= size && index >= 0){
+      return (E) elementData[index];
+    }
+    throw new IndexOutOfBoundsException();
+  }
+
+  /**
+   * Removes the element at the specified position in this list. Shifts any subsequent elements to
+   * the left (subtracts one from their indices).
+   *
+   * @param index the index of the element to be removed
+   * @return the element that was removed from the list
+   * @throws IndexOutOfBoundsException {@inheritDoc}
+   */
+  @Override
+  public E remove(int index) {
+    if (index <= size && index >= 0){
+      E oldVal = (E) elementData[index];
+
+      int numMoved = size - index - 1;
+      if (numMoved > 0)
+        System.arraycopy(elementData, index+1, elementData, index,
+            numMoved);
+      elementData[--size] = null;
+
+      return oldVal;
+    }
+    throw new IndexOutOfBoundsException();
+  }
+
+  /**
+   * Removes all of the elements from this list (optional operation). The list will be empty after
+   * this call returns.
+   */
+  @Override
+  public void clear() {
+    int i = size;
+    while(!this.isEmpty() && i >= 0){
+      this.remove(i);
+      i--;
+    }
+  }
+}
