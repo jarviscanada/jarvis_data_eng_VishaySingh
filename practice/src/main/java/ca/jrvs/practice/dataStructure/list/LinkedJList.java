40ad4ebac848363ffa8cb12a4554f1d1871dda93
Vishay Singh <vishaysingh35@gmail.com>
2022-06-01 18:08:19 +0000

    Add partial implementation of linked list

---
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/list/LinkedJList.java
@@ -0,0 +1,185 @@
+package ca.jrvs.practice.dataStructure.list;
+
+
+import java.util.LinkedList;
+
+public class LinkedJList<E> implements JList<E>{
+
+  public LinkedJList() {
+  }
+
+  private transient int size = 0;
+  /**
+   * Pointer to first node.
+   * Invariant: (first == null && last == null) ||
+   *            (first.prev == null && first.item != null)
+   */
+  transient Node<E> first;
+
+  /**
+   * Pointer to last node.
+   * Invariant: (first == null && last == null) ||
+   *            (last.next == null && last.item != null)
+   */
+  transient Node<E> last;
+
+  /**
+   * Appends the specified element to the end of this list (optional operation).
+   *
+   * @param e element to be appended to this list
+   * @return <tt>true</tt> (as specified by {Collection#add})
+   * @throws NullPointerException if the specified element is null and this list does not permit
+   *                              null elements
+   */
+  @Override
+  public boolean add(E e) {
+    final Node<E> l = last;
+    final Node<E> newNode = new Node<>(l, e, null);
+    last = newNode;
+    if (l == null)
+      first = newNode;
+    else
+      l.next = newNode;
+    size++;
+    return true;
+  }
+
+  /**
+   * Returns an array containing all of the elements in this list in proper sequence (from first to
+   * last element).
+   *
+   * <p>This method acts as bridge between array-based and collection-based
+   * APIs.
+   *
+   * @return an array containing all of the elements in this list in proper sequence
+   */
+  @Override
+  public Object[] toArray() {
+    Object[] result = new Object[size];
+    int i = 0;
+    for (Node<E> x = first; x != null; x = x.next)
+      result[i++] = x.item;
+    return result;
+  }
+
+  /**
+   * The size of the ArrayList (the number of elements it contains).
+   */
+  @Override
+  public int size() {
+    return size;
+  }
+
+  /**
+   * Returns <tt>true</tt> if this list contains no elements.
+   *
+   * @return <tt>true</tt> if this list contains no elements
+   */
+  @Override
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * Returns the index of the first occurrence of the specified element in this list, or -1 if this
+   * list does not contain the element. More formally, returns the lowest index <tt>i</tt> such
+   * that
+   * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>,
+   * or -1 if there is no such index.
+   *
+   * @param o
+   */
+  @Override
+  public int indexOf(Object o) {
+    int index = 0;
+    if (o == null) {
+      for (Node<E> x = first; x != null; x = x.next) {
+        if (x.item == null)
+          return index;
+        index++;
+      }
+    } else {
+      for (Node<E> x = first; x != null; x = x.next) {
+        if (o.equals(x.item))
+          return index;
+        index++;
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Returns <tt>true</tt> if this list contains the specified element. More formally, returns
+   * <tt>true</tt> if and only if this list contains at least one element <tt>e</tt> such that
+   * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+   *
+   * @param o element whose presence in this list is to be tested
+   * @return <tt>true</tt> if this list contains the specified element
+   * @throws NullPointerException if the specified element is null and this list does not permit
+   *                              null elements
+   */
+  @Override
+  public boolean contains(Object o) {
+    return this.indexOf(o) != -1;
+  }
+
+  /**
+   * Returns the element at the specified position in this list.
+   *
+   * @param index index of the element to return
+   * @return the element at the specified position in this list
+   * @throws IndexOutOfBoundsException if the index is out of range (<tt>index &lt; 0 || index &gt;=
+   *                                   size()</tt>)
+   */
+  @Override
+  public E get(int index) {
+    Node<E> x;
+    if (index >= 0 && index < size){
+      if (index < (size >> 1)) {
+        x = first;
+        for (int i = 0; i < index; i++)
+          x = x.next;
+      } else {
+        x = last;
+        for (int i = size - 1; i > index; i--)
+          x = x.prev;
+      }
+      return x.item;
+    }
+    throw new IndexOutOfBoundsException();
+  }
+
+  /**
+   * Removes the element at the specified position in this list. Shifts any subsequent elements to
+   * the left (subtracts one from their indices).
+   *
+   * @param index the index of the element to be removed
+   * @return the element that was removed from the list
+   * @throws IndexOutOfBoundsException {@inheritDoc}
+   */
+  @Override
+  public E remove(int index) {
+    return null;
+  }
+
+  /**
+   * Removes all of the elements from this list (optional operation). The list will be empty after
+   * this call returns.
+   */
+  @Override
+  public void clear() {
+
+  }
+
+  private static class Node<E> {
+    E item;
+    Node<E> next;
+    Node<E> prev;
+
+    Node(Node<E> prev, E element, Node<E> next) {
+      this.item = element;
+      this.next = next;
+      this.prev = prev;
+    }
+  }
+}
---
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/list/Node.java
@@ -0,0 +1,12 @@
+package ca.jrvs.practice.dataStructure.list;
+
+import java.util.LinkedList;
+
+public class Node<E> {
+
+  E item;
+
+  Node(E element) {
+    this.item = element;
+  }
+}
