0576ff892871f9b7c10bcf99c74ba499eabc0cf2
Vishay Singh <vishaysingh35@gmail.com>
2022-06-02 18:16:55 +0000

    Finish Binary Search

---
+++ practice/src/test/java/ca/jrvs/practice/search/BinarySearchTest.java
@@ -0,0 +1,35 @@
+package ca.jrvs.practice.search;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+  BinarySearch binarySearch = new BinarySearch();
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void binarySearchRecursion() {
+    Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Optional<Integer> index = binarySearch.binarySearchRecursion(arr, 3);
+    assertEquals(Optional.of(2), index);
+  }
+
+  @Test
+  public void binarySearchIteration() {
+    Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Optional<Integer> index = binarySearch.binarySearchIteration(arr, 3);
+    assertEquals(Optional.of(2), index);
+  }
+}
