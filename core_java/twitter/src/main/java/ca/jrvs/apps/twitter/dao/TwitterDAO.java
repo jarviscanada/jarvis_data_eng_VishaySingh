81373d5ec538f925446d6e6580feffdf592d0be7
Vishay Singh <vishaysingh35@gmail.com>
2022-06-10 21:29:15 +0000

    Update TwitterDAO

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterDAO.java
@@ -0,0 +1,62 @@
+package ca.jrvs.apps.twitter.dao;
+
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
+import ca.jrvs.apps.twitter.model.Tweet;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TwitterDAO implements CrdDao<Tweet, String> {
+
+  //URI Constants
+  private static final String API_BASE_URI = "https://api.twitter.com";
+  private static final String POST_PATH = "/1.1/statuses/update.json";
+  private static final String SHOW_PATH = "/1.1/statuses/show.json";
+  private static final String DELETE_PATH = "/1.1/statuses/destroy";
+
+  //URI Symbols
+  private static final String QUERY_SYM = "?";
+  private static final String AMPERSAND = "&";
+  private static final String EQUAL = "=";
+
+  //Response Code
+  private static final int HTTP_OK = 200;
+
+  private final HttpHelper httpHelper;
+
+  @Autowired
+  public TwitterDAO(HttpHelper httpHelper) {
+    this.httpHelper = httpHelper;
+  }
+
+  /**
+   * Create an entity(Tweet) to the underlying storage
+   *
+   * @param entity entity that to be created
+   * @return created entity
+   */
+  @Override
+  public Tweet create(Tweet entity) {
+    return null;
+  }
+
+  /**
+   * Find an entity(Tweet) by its id
+   *
+   * @param s entity id
+   * @return Tweet entity
+   */
+  @Override
+  public Tweet findById(String s) {
+    return null;
+  }
+
+  /**
+   * Delete an entity(Tweet) by its ID
+   *
+   * @param s of the entity to be deleted
+   * @return deleted entity
+   */
+  @Override
+  public Tweet deleteById(String s) {
+    return null;
+  }
+}
