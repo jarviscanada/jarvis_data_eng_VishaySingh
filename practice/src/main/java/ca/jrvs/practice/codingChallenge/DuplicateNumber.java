37cfc4c7a394de36ee517669d7e95863c190c8db
Vishay Singh <vishaysingh35@gmail.com>
2022-07-18 16:51:03 +0000

    Finish duplicate number

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/DuplicateNumber.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/DuplicateNumber.java
@@ -1,6 +1,8 @@
 package ca.jrvs.practice.codingChallenge;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * https://www.notion.so/jarvisdev/Find-the-Duplicate-Number-b092228364f04408ba9b55c243a589fb
@@ -10,6 +12,7 @@ public class DuplicateNumber {
   /**
    * Big-O Analysis: O(nlogn) worst-case time complexity due to sorting first. O(1) space worst-case
    * as we are only overwriting the array, and making a constant amount of space with prev.
+   *
    * @param nums
    * @return
    */
@@ -23,6 +26,24 @@ public class DuplicateNumber {
       prev = nums[i];
     }
     return prev;
+  }
+
+  /**
+   * Big-O Analysis: O(n) worst-case time complexity. O(n) worst-case space complexity.
+   *
+   * @param nums
+   * @return
+   */
+  public int findDuplicateSet(int[] nums) {
+    Set<Integer> hashSet = new HashSet<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      if (hashSet.contains(nums[i])) {
+        return nums[i];
+      }
+      hashSet.add(nums[i]);
+    }
+    return nums[0];
   }
 
 }
